7ec368fde574633dc2080598fab8a7a5bfb692dc
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-10-16 19:51:35 +0200

    Improve filtering of ROS DDS files

--- src/auto_abi_checker/ros_utils.py
+++ src/auto_abi_checker/ros_utils.py
@@ -9,13 +9,16 @@ from shutil import rmtree
 
 
 def remove_dds_files(directory, dds_impl):
-    for filename in Path(directory).glob('**/*__rosidl_typesupport_' + dds_impl + '_cpp.h*'):
+    for filename in Path(directory).glob('**/*rosidl_typesupport_' + dds_impl + '*.h*'):
+        remove(filename)
+    for filename in Path(directory).glob('**/*rosidl_typesupport_' + dds_impl + '*.so'):
         remove(filename)
     for dirname in Path(directory).glob('**/*srv/dds_' + dds_impl):
         rmtree(dirname)
     for dirname in Path(directory).glob('**/*msg/dds_' + dds_impl):
         rmtree(dirname)
-
+    for dirname in Path(directory).glob('**/*msg/dds_' + dds_impl + "_c"):
+        rmtree(dirname)
 
 def clean_non_default_dss_files(directory):
     remove_dds_files(directory, 'opensplice')
